b0ad6be74ac2b43dde160ecec19743990bfefb12
soterapablo <soterapablo@gmail.com>
2026-05-25 12:36:50 -0300

    fix: types in Framer Motion variants

--- components/Services.tsx
+++ components/Services.tsx
@@ -60,7 +60,7 @@ const containerVariants = {
 
 const itemVariants = {
   hidden: { opacity: 0, y: 30 },
-  visible: { opacity: 1, y: 0, transition: { type: "spring", stiffness: 100 } }
+  visible: { opacity: 1, y: 0, transition: { type: "spring" as const, stiffness: 100 } }
 };
 
 export default function Services() {
